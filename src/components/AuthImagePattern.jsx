9fd31a4905463704c0bac6a820b35bd6da11694d
qinanagao@gmail.com <qinanagao@gmail.com>
2025-08-06 23:11:58 +0100

    tweak ui

--- src/components/AuthImagePattern.jsx
+++ src/components/AuthImagePattern.jsx
@@ -1,8 +1,8 @@
 function AuthImagePattern({ title, subtitle }) {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+    <div className="hidden lg:flex items-center justify-center bg-base-200 pt-[70px]">
       <div className="max-w-md text-center">
-        <div className="grid grid-cols-3 gap-3 mb-8">
+        <div className="grid grid-cols-3 gap-3 mb-8 ">
           {[...Array(9)].map((_, i) => (
             <div
               key={i}
